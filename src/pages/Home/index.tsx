16532f80c46399e402581982a7650012a244cc6d
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-09-16 15:53:08 -0300

    chore: add luxon lib

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import Menu from "components/Menu";
+import { DateTime } from "luxon";
 import { ReactComponent as Search } from "assets/icons/search.svg";
 import * as S from "./style";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
 
 const Home = () => {
+    const dateDescription = DateTime.now().toLocaleString({
+        ...DateTime.DATE_SHORT,
+        weekday: "long",
+      });
     return(
         <S.Home>
             <Menu 
@@ -15,7 +20,7 @@ const Home = () => {
                     <S.HomeHeaderDetails>
                         <div>
                             <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
-                            <S.HomeHeaderDetailsDate>Aqui ficará a data</S.HomeHeaderDetailsDate>
+                            <S.HomeHeaderDetailsDate>{dateDescription}</S.HomeHeaderDetailsDate>
                         </div>
                         <S.HomeHeaderDetailsSearch>
                             <Search/>
