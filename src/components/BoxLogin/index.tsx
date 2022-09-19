65fe9f7351a303fbb63f3428c6c46f701f9e992d
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-09-19 17:02:07 -0300

    chore: add component ButtonLarge and style

--- src/components/BoxLogin/index.tsx
+++ src/components/BoxLogin/index.tsx
@@ -1,5 +1,6 @@
 import * as S from "./style";
 import logo from 'assets/imgs/logo.png';
+import ButtonLarge from "components/ButtonLarge";
 const BoxLogin = ()=> {
     return(
         <S.BoxLogin>
@@ -14,6 +15,7 @@ const BoxLogin = ()=> {
                 <input type="text" placeholder="E-mail"/>
                 <input type="password" placeholder="Senha"/>
             </S.BoxLoginForm>
+            <ButtonLarge value="Entrar" type="button" />
         </S.BoxLogin>
     );
 }
