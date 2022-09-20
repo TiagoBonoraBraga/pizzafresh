9ce0336dd8a71385103cd30769ca066924ce591a
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-09-20 14:41:59 -0300

    chore: add component ButtonToggle and style

--- src/components/OrderDetails/index.tsx
+++ src/components/OrderDetails/index.tsx
@@ -1,3 +1,4 @@
+import ButtonToggle from "components/ButtonToggle";
 import * as S from "./style";
 
 const OrderDetails = () => {
@@ -5,7 +6,9 @@ const OrderDetails = () => {
         <S.OrderDetails>
             <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
             <S.OrderDetailsButtonGroup>
-
+                <ButtonToggle active={true} value="Comer no Local"/>
+                <ButtonToggle active={false} value="P/ Viagem"/>
+                <ButtonToggle active={true} value="Delivery"/>
             </S.OrderDetailsButtonGroup>
             <S.OrderDetailsList>
                 <p>Itens do Pedido</p>
