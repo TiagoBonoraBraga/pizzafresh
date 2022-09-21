0f4ee4b18a8ca6ccd47ec00bf25c9d49c253cde6
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-09-20 21:03:58 -0300

    chore: add Settings, style and NavColumn basic

---
+++ src/components/NavColumn/index.tsx
@@ -0,0 +1,7 @@
+import * as S from "./style";
+
+const NavColumn = () => {
+    return ();
+}
+
+export default NavColumn;
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
-import Home from "pages/Home";
+// import Home from "pages/Home";
 import Login from "pages/Login";
+import Settings from "pages/Settings";
 
 
 const Router = () => {
     return(
         <Routes>
             <Route path={RoutePath.LOGIN} element={<Login />} />
-            <Route path={RoutePath.HOME} element={<Home />} />
+            <Route path={RoutePath.HOME} element={<Settings />} />
         </Routes>
     );
 }
