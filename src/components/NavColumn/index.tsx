0146859a29bbbfd02f3a2bc3c2becf108b122719
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-09-22 12:17:28 -0300

    chore: add ManageTable component and style

--- src/components/NavColumn/index.tsx
+++ src/components/NavColumn/index.tsx
@@ -18,7 +18,7 @@ const NavColumn = ({activeRoute}: NavColumnProps) => {
             icon: <Market />,
             title: "Customize suas mesas",
             subtitle: "Adicione mesas, configure nomes",
-            navigation: RoutePath.SETTINGS_TABLE,
+            navigation: RoutePath.SETTINGS_TABLES,
         },
         {
             icon: <Info />,
