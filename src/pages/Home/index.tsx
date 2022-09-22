9bcd3390bfd6a42f3c229a7a07ade9bd3723408b
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-09-22 14:01:06 -0300

    chore: add renderização de daos mock

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,6 +10,9 @@ import OrderDetails from "components/OrderDetails";
 import Overlay from "components/Overlay";
 import CheckoutSection from "components/CheckoutSection";
 import { useNavigate } from "react-router-dom";
+import { products } from "mocks/products";
+import { orders } from "mocks/orders";
+import { ProductResponse } from "types/Product";
 
 const Home = () => {
     const dateDescription = DateTime.now().toLocaleString({
@@ -19,6 +22,7 @@ const Home = () => {
 
     const navigate = useNavigate();
     const handleNavigation = (path: RoutePath) => navigate(path);
+    const handleSelection = (product: ProductResponse) => {}
 
     return(
         <S.Home>
@@ -47,7 +51,14 @@ const Home = () => {
                     </S.HomeProductTitle>
                     <S.HomeProductList>
                         <ProductItemList>
-                            <ProductItem />
+                            {Boolean(products.length) &&
+                            products.map((product, index) => (
+                            <ProductItem 
+                                product={product}
+                                key={`ProductItem-${index}`}
+                                onSelect={handleSelection}
+                            />
+                            ))}
                         </ProductItemList>
                     </S.HomeProductList>
                 </div>
