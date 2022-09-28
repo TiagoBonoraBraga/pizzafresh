4e21b1c5ddb6492f6a3ed32036c0ead032cbf436
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-09-28 15:41:26 -0300

    chore: add Manage Products (adição)

--- src/components/ManageProducts/index.tsx
+++ src/components/ManageProducts/index.tsx
@@ -1,52 +1,150 @@
 import { ReactComponent as Add } from "assets/icons/add.svg";
 import EditProduct from "components/EditProduct";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useState, useEffect } from "react";
+import { useMutation, useQuery } from "react-query";
+import { ProductService } from "services/ProductService";
+import { ErrorResponse } from "types/api/error";
+import { Product, ProductResponse } from "types/api/product";
+import { QueryKey } from "types/QueryKey";
 import * as S from "./style";
 
 type ManageProductsType = HTMLAttributes<HTMLDivElement>;
 
 type ManageProductsProps = {} & ManageProductsType;
 
+const ManageProducts = ({ ...props }: ManageProductsProps) => {
+  const [products, setProducts] = useState<ProductResponse[]>([]);
 
+  const { data: productsData } = useQuery(
+    [QueryKey.PRODUCTS],
+    ProductService.getLista
+  );
 
-const ManageProducts = ({...props}: ManageProductsProps ) => {
-    return (
-        <S.ManageProducts {...props}>
-            <S.ManageProductsTitle>Gerenciar Produtos</S.ManageProductsTitle>
-            <S.ManageProductsSub>
-                <b>Pizzas</b>
-            </S.ManageProductsSub>
-            <S.ManageProductsContent>
-                <S.ManageProductsContentAdd>
-                    <Add />
-                    <span>Adicionar Pizza</span>
-                </S.ManageProductsContentAdd>
-                <S.AddCard>
-                    <S.EditForm 
-                        type="text"
-                        placeholder="Título"
-                    />
-                    <S.EditForm 
-                         type="number"
-                         placeholder="Preço"
-                    />
-                    <S.EditForm
-                         type="text"
-                         placeholder="Descrição"
-                    />
-                    <S.EditForm 
-                         type="url"
-                         placeholder="Imagem"
-                    />
-                </S.AddCard>
-                <EditProduct />
-            </S.ManageProductsContent>
-            <S.ManageProductsActions>
-                <S.ManageProductsActionsCancel>Cancelar</S.ManageProductsActionsCancel>
-                <S.ManageProductsActionsSave>Salvar Mudanças</S.ManageProductsActionsSave>
-            </S.ManageProductsActions>
-        </S.ManageProducts>
+  const add = useMutation(ProductService.create, {
+    onSuccess: (data: ProductResponse & ErrorResponse) => {
+      if (data.statusCode) {
+        return;
+      }
+
+      const productList = [...products, data as ProductResponse];
+      setProducts(productList);
+    },
+    onError: () => {
+      console.error("Erro ao adicionar um produto");
+    },
+  });
+
+  const form = {
+    name: "",
+    price: Number(""),
+    image: "",
+    description: "",
+  };
+
+  const [isAdding, setIsAdding] = useState(false);
+  const [productToAdd, setProductToAdd] = useState(form);
+
+  const handleAddChange = (name: string, value: string | number) => {
+    setProductToAdd({ ...productToAdd, [name]: value });
+  };
+
+  const productIsValid = () =>
+    Boolean(
+      productToAdd.name.length &&
+        productToAdd.price.toString().length &&
+        productToAdd.description.length &&
+        productToAdd.image.length
     );
-}
+
+  const productFormatter = (toFormat: typeof form): Product => ({
+    name: toFormat.name,
+    price: toFormat.price,
+    description: toFormat.description,
+    image: toFormat.image,
+  });
+
+  const [cancel, setCancel] = useState(false);
+
+  const handleCancel = () => {
+    setCancel(true);
+    setIsAdding(false);
+    setTimeout(() => setCancel(false));
+  };
+
+  const handleSave = () => {
+    const canAdd = productIsValid();
+    const productFormatted = productFormatter(productToAdd);
+
+    if (canAdd) add.mutate(productFormatted);
+    setTimeout(() => handleCancel(), 300);
+    setProductToAdd(form);
+    setIsAdding(false);
+  };
+  
+  useEffect(() => {
+    setProducts(productsData || []);
+  }, [productsData]);
+
+  return (
+    <S.ManageProducts {...props}>
+      <S.ManageProductsTitle>Gerenciar Produtos</S.ManageProductsTitle>
+      <S.ManageProductsSub>
+        <b>Pizzas</b>
+      </S.ManageProductsSub>
+      <S.ManageProductsContent>
+        {!isAdding ? (
+          <S.ManageProductsContentAdd onClick={() => setIsAdding(!isAdding)}>
+            <Add />
+            <span>Adicionar Pizza</span>
+          </S.ManageProductsContentAdd>
+        ) : (
+          <S.AddCard>
+            <S.EditForm
+              type="text"
+              placeholder="Título"
+              success={Boolean(productToAdd.name.length)}
+              value={productToAdd.name}
+              onChange={({ target }) => handleAddChange("name", target.value)}
+            />
+            <S.EditForm
+              type="number"
+              placeholder="Preço"
+              success={Boolean(productToAdd.price)}
+              value={productToAdd.price || ""}
+              onChange={({ target }) => handleAddChange("price", target.value)}
+            />
+            <S.EditForm
+              type="text"
+              placeholder="Descrição"
+              success={Boolean(productToAdd.description.length)}
+              value={productToAdd.description}
+              onChange={({ target }) =>
+                handleAddChange("description", target.value)
+              }
+            />
+            <S.EditForm
+              type="url"
+              placeholder="Imagem"
+              success={Boolean(productToAdd.image.length)}
+              value={productToAdd.image}
+              onChange={({ target }) => handleAddChange("image", target.value)}
+            />
+          </S.AddCard>
+        )}
+        {products.map((product, index) => (
+          <EditProduct product={product} key={index} />
+        ))}
+      </S.ManageProductsContent>
+      <S.ManageProductsActions>
+        <S.ManageProductsActionsCancel onClick={handleCancel}>
+          Cancelar
+        </S.ManageProductsActionsCancel>
+        <S.ManageProductsActionsSave onClick={handleSave}>
+          Salvar Mudanças
+        </S.ManageProductsActionsSave>
+      </S.ManageProductsActions>
+    </S.ManageProducts>
+  );
+};
 
 export default ManageProducts;
